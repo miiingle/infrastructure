425fb7f01aca6e267e5f24da0891b79f67a36daf
LM Bibera <lyndon.bibera@headhuntr.io>
2021-01-31 17:33:51 +0800

    add an alarm to check if theres too many active connections

--- rds/rds-alarms.tf
+++ rds/rds-alarms.tf
@@ -14,3 +14,19 @@ resource "aws_cloudwatch_metric_alarm" "free_storage_space_too_low" {
     DBInstanceIdentifier = aws_db_instance.db_transaction.id
   }
 }
+
+resource "aws_cloudwatch_metric_alarm" "connection_usage_too_high" {
+  alarm_name          = "${aws_db_instance.db_transaction.identifier}_connection_usage_too_high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "DatabaseConnections"
+  namespace           = "AWS/RDS"
+  period              = "600"
+  statistic           = "Average"
+  threshold           = 70
+  alarm_description   = "Average database connection usage over last 10 minutes too high, performance may suffer"
+
+  dimensions = {
+    DBInstanceIdentifier = aws_db_instance.db_transaction.id
+  }
+}
