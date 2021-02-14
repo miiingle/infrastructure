5706d36286b2e6bee0b35d7546c85e11a79b931a
LM Bibera <lyndon.bibera@headhuntr.io>
2021-02-14 20:29:09 +0800

    Fix the cleanup script

--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -1,10 +1,10 @@
 aws eks update-kubeconfig --name miiingle-dev-eks && \
 kubectl delete svc miiingle-dev-eks-loadbalancer && \
-terraform state rm 'module.eks.kubernetes_service.load_balancer'
-terraform state rm 'module.eks.kubernetes_namespace.cloudwatch'
-terraform state rm 'module.eks.kubernetes_secret.all_secrets'
-terraform state rm 'module.eks.helm_release.application_user_api'
-terraform state rm 'module.eks.helm_release.cloudwatch_utilities'
-terraform state rm 'module.eks.helm_release.cluster_autoscaler'
-terraform state rm 'module.eks.module.eks_cluster.kubernetes_config_map.aws_auth[0]'
+terraform state rm 'module.kubernetes.kubernetes_service.load_balancer'
+terraform state rm 'module.kubernetes.kubernetes_namespace.cloudwatch'
+terraform state rm 'module.kubernetes.kubernetes_secret.all_secrets'
+terraform state rm 'module.kubernetes.helm_release.application_user_api'
+terraform state rm 'module.kubernetes.helm_release.cloudwatch_utilities'
+terraform state rm 'module.kubernetes.helm_release.cluster_autoscaler'
+terraform state rm 'module.kubernetes.module.eks_cluster.kubernetes_config_map.aws_auth[0]'
 terraform destroy --auto-approve
