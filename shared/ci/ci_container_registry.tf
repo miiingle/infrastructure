14958f463bd4040f1b6e66ab74a5e9dae5ad232f
LM Bibera <lyndon.bibera@headhuntr.io>
2021-02-19 12:22:01 +0800

    ecr registry for graal image

--- shared/ci/ci_container_registry.tf
+++ shared/ci/ci_container_registry.tf
@@ -1,3 +1,7 @@
 resource "aws_ecr_repository" "user_api" {
   name = "${var.project_prefix}.user_api"
 }
+
+resource "aws_ecr_repository" "oracle_graalvm" {
+  name = "${var.project_prefix}.oracle_graalvm"
+}
