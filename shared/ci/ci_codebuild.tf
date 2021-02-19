3210c2185c0a554fa8b5aed45e7daa2c861954ce
LM Bibera <lyndon.bibera@headhuntr.io>
2021-02-20 06:41:56 +0800

    oops, too expensive

--- shared/ci/ci_codebuild.tf
+++ shared/ci/ci_codebuild.tf
@@ -13,8 +13,8 @@ resource "aws_codebuild_project" "user_api" {
   }
 
   environment {
-    compute_type                = "BUILD_GENERAL1_2XLARGE"
-    image                       = "aws/codebuild/standard:5.0"
+    compute_type                = "BUILD_GENERAL1_LARGE"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = true
